53fbcec44589f788ff5ce1a1ccbd32f5f01f32f4
cesaranil <8182cesar17@gmail.com>
2022-08-10 09:00:24 -0500

    Aprendiendo sobre el ciclo while

---
+++ while.js
@@ -0,0 +1,19 @@
+/*while(Condicion){
+    //codigo
+}*/
+
+// Ciclo del 1 al 10
+/*var numero = 1;
+while(numero<=10){
+    document.write(numero + '<br>');
+    numero++;
+}*/
+
+//Ciclo do while, permite que el codigo se ejecute al menos una vez
+var numero = 1;
+do{
+    document.write(numero);
+    document.write('<br>');
+
+    numero++;
+}while(numero<=10);
