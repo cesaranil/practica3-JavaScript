a6f74ef64d5a5dd17ac7fb14d00117aca2465ddf
cesaranil <8182cesar17@gmail.com>
2022-08-10 08:46:12 -0500

    Aprendiendo sobre los ciclos for

--- for.js
+++ for.js
@@ -4,7 +4,29 @@ for(inicio, condicion, actualizacion){
 }
 */
 
-for (var i= 0; i < 10; i++ ) {
+// Ciclo del 1 al 10
+/*for (var i= 0; i < 10; i++ ) {
     document.write(i);
     document.write('<br >');   
-}
+}*/
+
+//Ciclo del 10 al 1
+/*for (var i = 9; i >= 0; i--) {
+    document.write(i);
+    document.write('<br>');   
+}*/
+
+//Ejemplo recorriendo un arreglo
+
+var meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto',
+            'Septiembre','Octubre','Noviembre','Diciembre'];
+
+/*            for(var i = 0; i < meses.length; i++){
+                document.write(i+1 + ') ' + meses[i]);
+                document.write('<br>');
+            }*/
+
+            for (mes in meses) {
+                document.write(meses[mes]);
+                document.write('<br>');
+            }
