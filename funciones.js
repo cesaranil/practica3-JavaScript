1d69755ba2261e7c8a158afd37115c331af3c913
cesaranil <8182cesar17@gmail.com>
2022-08-11 16:46:43 -0500

    Aprendiendo sobre funciones

---
+++ funciones.js
@@ -0,0 +1,31 @@
+/*function suma(numero1, numero2){
+    document.write(numero1 + numero2);
+    document.write("<br>");
+}
+
+suma (10, 5);
+suma (7, 3);*/
+
+// RETURN 
+
+/*function suma(numero1, numero2){
+    var resultado = numero1 * numero2;
+    return resultado;
+}
+
+document.write(suma(3,7));*/
+
+
+
+/*var suma = function(numero1, numero2){
+    return numero1 + numero2;
+}
+
+document.write(suma(8, 7));*/
+
+(function(){
+    alert("Hola Mundo");
+}());
+
+
+
