9aa7ed09c6707b384181ec19a127fdba3c962afa
cesaranil <8182cesar17@gmail.com>
2022-08-11 21:50:35 -0500

    Practicando metodos de cadenas de texto

---
+++ metodos-cadenas.js
@@ -0,0 +1,27 @@
+//Metodo length
+/*var nombre = "Cesar Orellana";
+var cantidad = nombre.length;
+document.write(cantidad);*/
+
+//Metodo substring
+/*var nombre = "Cesar Orellana";
+var apellido = nombre.substring(6, 14);
+document.write(apellido);*/
+
+//Metodo substr aqui es muy parecido al anterior solo que en el 
+//segundo valor indicamos cuantos caracteres queremos tener
+/*var nombre = "Cesar Orellana";
+var apellido = nombre.substr(6, 2);
+document.write(apellido);*/
+
+//Metodo replace
+/*var nombre = "Cesar Orellana";
+var nombre2 = nombre.replace('Cesar', 'Anibal');
+document.write(nombre2);*/
+
+//Método toUpperCase
+//var nombre = "cesar orellana";
+//nombre = nombre.toUpperCase();
+//nombre = nombre.toLowerCase()
+//document.write(nombre);
+
