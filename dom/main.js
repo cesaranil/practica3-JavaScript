a68a18761318b26874801310831890160a907ec2
cesaranil <8182cesar17@gmail.com>
2022-08-12 21:19:28 -0500

    Haciendo las primeras practicas con el DOM

---
+++ dom/main.js
@@ -0,0 +1,10 @@
+//var cajas = document.getElementsByTagName('div');
+//var cajas = document.getElementsByClassName('caja');
+
+var caja1 = document.getElementById('primeracaja');
+caja1.textContent = "Soy la Caja 1";
+
+//cajas[0].textContent = 'Hola Mundo';
+//cajas[1].innerHTML = '<h1>Prueba</h1>';
+
+ 
