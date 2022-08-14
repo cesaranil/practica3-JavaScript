e6727e0201f9ee459c0d4b9d1f6822092fdf3264
cesaranil <8182cesar17@gmail.com>
2022-08-13 21:35:48 -0500

    Aprendiendo a como agregar nodos al DOM

--- dom/main.js
+++ dom/main.js
@@ -7,4 +7,13 @@ caja1.textContent = "Soy la Caja 1";
 //cajas[0].textContent = 'Hola Mundo';
 //cajas[1].innerHTML = '<h1>Prueba</h1>';
 
+//==== Creando Nodos =====//
+var caja = document.createElement('div');
+var contendio = document.createTextNode('Hola Mundo');
+caja.appendChild(contendio);
+caja.setAttribute('class', 'caja naranja');
+
+var contenedor = document.getElementById('contenedor');
+contenedor.appendChild(caja);
+
  
