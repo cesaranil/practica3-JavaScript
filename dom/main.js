a3e1a83264dec79b8ddc7f01948e3307f58f2c40
cesaranil <8182cesar17@gmail.com>
2022-08-15 20:30:28 -0500

    Aprendiendo como Modofocar, Reemplazar y Eliminar un nodo en el DOM

--- dom/main.js
+++ dom/main.js
@@ -1,4 +1,4 @@
-//var cajas = document.getElementsByTagName('div');
+var cajas = document.getElementsByTagName('div');
 //var cajas = document.getElementsByClassName('caja');
 
 var caja1 = document.getElementById('primeracaja');
@@ -11,9 +11,24 @@ caja1.textContent = "Soy la Caja 1";
 var caja = document.createElement('div');
 var contendio = document.createTextNode('Hola Mundo');
 caja.appendChild(contendio);
-caja.setAttribute('class', 'caja naranja');
+//caja.setAttribute('class', 'caja naranja');
 
 var contenedor = document.getElementById('contenedor');
 contenedor.appendChild(caja);
+
+//==== Modificando la Clase o id de un elemento =====//
+
+ //otra forma de modificar el id y la clase
+ caja.id = 'primera';
+ caja.className = 'caja naranja';
+
+ // === Conociendo el elemnto padre === //
+ var padre = cajas[0].parentNode;
+ //padre.insertBefore(caja, caja1);
+
+ padre.replaceChild(caja, cajas[1]);
+
+ //como eliminar nodos
+ padre.removeChild(cajas[3]);
 
  
